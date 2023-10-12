d1dda322f11f893a484c5a35fdbb17e2b357d9c7
lWaterLite <lwaterlite@126.com>
2023-10-12 16:00:00 +0800

    complete webtransport echo

--- application/app.py
+++ application/app.py
@@ -16,6 +16,27 @@ async def index(request):
     return HTMLResponse(html.read())
 
 
+async def wt(scope, receive: Receive, send: Send) -> None:
+    message = await receive()
+    assert message['type'] == 'webtransport.connect'
+    await send({'type': 'webtransport.accept'})
+
+    while True:
+        message = await receive()
+        print(message)
+        if message['type'] == 'webtransport.datagram.receive':
+            await send({
+                'type': 'webtransport.datagram.send',
+                'data': message['data']
+            })
+        elif message['type'] == 'webtransport.stream.receive':
+            await send({
+                'type': 'webtransport.stream.send',
+                'data': message['data'],
+                'stream': message['stream']
+            })
+
+
 starletteApp = Starlette(routes=[
     Route('/', index),
     Mount('/', StaticFiles(directory=TEMPLATE_ROOT, html=True))
@@ -23,4 +44,7 @@ starletteApp = Starlette(routes=[
 
 
 async def app(scope: Scope, receive: Receive, send: Send) -> None:
-    await starletteApp(scope, receive, send)
+    if scope['type'] == 'webtransport' and scope['path'] == '/wt':
+        await wt(scope, receive, send)
+    else:
+        await starletteApp(scope, receive, send)
--- server_packet/custom_handler.py
+++ server_packet/custom_handler.py
@@ -2,21 +2,21 @@ import asyncio
 
 from aioquic import __version__
 from aioquic.asyncio import QuicConnectionProtocol
-from aioquic.h3.connection import H3Connection, H3Event, DataReceived, HeadersReceived, H3_ALPN
+from aioquic.h3.connection import H3Connection, H3Event, DataReceived, HeadersReceived, H3_ALPN, DatagramReceived, WebTransportStreamDataReceived
 from aioquic.h3.exceptions import NoAvailablePushIDError
 from aioquic.quic.events import QuicEvent, ProtocolNegotiated
-from typing import Dict, Callable, cast, Optional, Union
+from typing import Dict, Callable, cast, Optional, Union, Deque
 from email.utils import formatdate
 from time import time
+from collections import deque
 
 from application.app import app
 
-SERVER_NAME = 'WebOBS.aioquic/' + __version__
+SERVER_NAME = 'webobs.aioquic/' + __version__
 
 
 class HttpHandler:
     def __init__(self,
-                 *,
                  authority: bytes,
                  connection: H3Connection,
                  protocol: QuicConnectionProtocol,
@@ -91,7 +91,84 @@ class HttpHandler:
         await app(self.scope, self.receive, self.send)
 
 
-BaseHandler = Union[HttpHandler]
+class WebTransportHandler:
+    def __init__(self,
+                 connection: H3Connection,
+                 scope: Dict,
+                 stream_id: int,
+                 transmit: Callable[[], None]) -> None:
+        self.connection = connection
+        self.scope = scope
+        self.stream_id = stream_id
+        self.transmit = transmit
+        self.queue = asyncio.Queue()
+        self.http_event_queue = deque()
+        self.accepted = False
+        self.closed = False
+
+    def http_event_received(self, event: H3Event) -> None:
+        if not self.closed:
+            if self.accepted:
+                if isinstance(event, DatagramReceived):
+                    self.queue.put_nowait({
+                        'type': 'webtransport.datagram.receive',
+                        'data': event.data
+                    })
+                elif isinstance(event, WebTransportStreamDataReceived):
+                    self.queue.put_nowait({
+                        'type': 'webtransport.stream.receive',
+                        'stream': event.stream_id,
+                        'data': event.data
+                    })
+            else:
+                self.http_event_queue.append(event)
+
+    async def receive(self) -> Dict:
+        return await self.queue.get()
+
+    async def send(self, message: Dict) -> None:
+        data = b''
+        end_stream = False
+
+        if message['type'] == 'webtransport.accept':
+            self.accepted = True
+            headers = [
+                (b':status', b'200'),
+                (b'server', SERVER_NAME.encode()),
+                (b'date', formatdate(time(), usegmt=True).encode()),
+                (b'sec-webtransport-http3-draft', b'draft02')
+            ]
+            self.connection.send_headers(stream_id=self.stream_id, headers=headers)
+            while self.http_event_queue:
+                self.http_event_received(self.http_event_queue.popleft())
+        elif message['type'] == 'webtransport.close':
+            if not self.accepted:
+                self.connection.send_headers(stream_id=self.stream_id, headers=[(b':status', b'403')])
+            end_stream = True
+        elif message['type'] == 'webtransport.datagram.send':
+            self.connection.send_datagram(flow_id=self.stream_id, data=message['data'])
+        elif message['type'] == 'webtransport.stream.send':
+            self.connection._quic.send_stream_data(stream_id=message['stream'], data=message['data'])
+
+        if data or end_stream:
+            self.connection.send_data(stream_id=self.stream_id, data=data, end_stream=end_stream)
+
+        if end_stream:
+            self.closed = True
+
+        self.transmit()
+
+    async def run_asgi(self, app) -> None:
+        self.queue.put_nowait({'type': 'webtransport.connect'})
+
+        try:
+            await app(self.scope, self.receive, self.send)
+        finally:
+            if not self.closed:
+                await self.send({'type': 'webtransport.close'})
+
+
+BaseHandler = Union[HttpHandler, WebTransportHandler]
 
 
 class HttpServerProtocol(QuicConnectionProtocol):
@@ -104,6 +181,7 @@ class HttpServerProtocol(QuicConnectionProtocol):
         if isinstance(event, HeadersReceived) and event.stream_id not in self._handlers:
             authority = None
             headers = []
+            http_version = '3'
             raw_path = b''
             method = ''
             protocol = None
@@ -120,12 +198,11 @@ class HttpServerProtocol(QuicConnectionProtocol):
                     headers.append((header, value))
 
             if b'?' in raw_path:
-                deq_path, query_string = raw_path.split(b'?', maxsplit=1)
+                path, query_string = raw_path.split(b'?', maxsplit=1)
             else:
-                deq_path, query_string = raw_path, b''
-            deq_path = deq_path.decode()
-            print(f'HTTP request {method} {deq_path}')
-            self._quic._logger.info(f'HTTP request {method} {deq_path}')
+                path, query_string = raw_path, b''
+            path = path.decode()
+            print(f'HTTP request {method} {path}')
 
             client_addr = self._http._quic._network_paths[0].addr
             client = (client_addr[0], client_addr[1])
@@ -134,10 +211,27 @@ class HttpServerProtocol(QuicConnectionProtocol):
             scope: Dict
             if method == 'CONNECT' and protocol == 'websocket':
                 # TODO WebSocket
-                pass
+                return
             elif method == 'CONNECT' and protocol == 'webtransport':
                 # TODO WebTransport
-                pass
+                scope = {
+                    'client': client,
+                    'headers': headers,
+                    'http_version': http_version,
+                    'method': method,
+                    'path': path,
+                    'query_string': query_string,
+                    'raw_path': raw_path,
+                    'root_path': '',
+                    'scheme': 'https',
+                    'type': 'webtransport'
+                }
+                handler = WebTransportHandler(
+                    connection=self._http,
+                    scope=scope,
+                    stream_id=event.stream_id,
+                    transmit=self.transmit
+                )
             else:
                 extensions: Dict[str, Dict] = {}
                 if isinstance(self._http, H3Connection):
@@ -147,9 +241,9 @@ class HttpServerProtocol(QuicConnectionProtocol):
                     'client': client,
                     'extensions': extensions,
                     'headers': headers,
-                    'http_version': '3',
+                    'http_version': http_version,
                     'method': method,
-                    'path': deq_path,
+                    'path': path,
                     'query_string': query_string,
                     'raw_path': raw_path,
                     'root_path': '',
@@ -159,10 +253,18 @@ class HttpServerProtocol(QuicConnectionProtocol):
                 handler = HttpHandler(authority=authority, connection=self._http, protocol=self, scope=scope,
                                       stream_id=event.stream_id, stream_ended=event.stream_ended,
                                       transmit=self.transmit)
-                self._handlers[event.stream_id] = handler
-                asyncio.ensure_future(handler.run_asgi(app))
+
+            self._handlers[event.stream_id] = handler
+            asyncio.ensure_future(handler.run_asgi(app))
+
         elif isinstance(event, (DataReceived, HeadersReceived)) and event.stream_id in self._handlers:
             handler = self._handlers[event.stream_id]
+            handler.http_event_received(event)
+        elif isinstance(event, DatagramReceived):
+            handler = self._handlers[event.flow_id]
+            handler.http_event_received(event)
+        elif isinstance(event, WebTransportStreamDataReceived):
+            handler = self._handlers[event.session_id]
             handler.http_event_received(event)
 
     def quic_event_received(self, event: QuicEvent) -> None:
